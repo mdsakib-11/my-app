5a468092aff57a66731cd4ed8aa737452dabfdaf
mdsakib-11 <sakibbirol44@gmail.com>
2022-10-01 11:38:19 +0600

    Css Style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css'
+const title = "Bangladesh";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h2>Hello</h2>
+  <h2 className='headerStyle'>{title}</h2>
 );
 
